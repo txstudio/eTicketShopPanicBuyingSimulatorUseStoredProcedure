bc83e38d3d18c48c9710b27f2fa13806950a19eb
Shi Hao Wang <txstudio@outlook.com>
2018-11-15 14:24:38 +0800

    初始化負載測試專案框架

---
+++ app/eTicketLoader/eTicketLoaderApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace eTicketLoaderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
