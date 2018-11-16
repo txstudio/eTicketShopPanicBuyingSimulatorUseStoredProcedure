252824cb8e3a2d03f350535a8186ab92f59bf295
Shi Hao Wang <txstudio@outlook.com>
2018-11-16 10:12:30 +0800

    加入事件紀錄資料物件

---
+++ app/eTicketLoader/eTicketLoader.Data/Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eTicketLoader.Data
+{
+    public sealed class Log
+    {
+        public int No { get; set; }
+        public Nullable<DateTimeOffset> EventDateTime { get; set; }
+
+        public Nullable<Guid> MemberGUID { get; set; }
+
+        public int TicketNumber { get; set; }
+        public decimal? ListPrice { get; set; }
+
+        public int? Elapsed { get; set; }
+        public bool? IsSuccess { get; set; }
+
+        public string Exception { get; set; }
+        public int? Retry { get; set; }
+    }
+}
